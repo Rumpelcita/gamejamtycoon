20f3248c4b158f58adafc5812ab7602b9479db13
Rumpelcita <rumpelcita@gmail.com>
2015-01-24 10:29:43 -0300

    Project setup

---
+++ src/js/characters.js
@@ -0,0 +1,44 @@
+var characters = [	
+	{
+		name: "generic1",
+		description: "regular guy, no special effects, owns a laptop",
+		skills: {gameDesign: 1,
+				 programming: 1,
+				 graphics: 1,
+				 music: 1		 
+				},
+		needs: {hunger: 50,
+				thirst: 50,
+				sleep: 50,
+				stress: 50,
+				focus: 50
+				},
+		personality: {leadership: 10,
+					  creativity: 10,
+					  hungerEnd: 50,
+					  thirstEnd: 50,
+				      sleepEnd: 50,
+				      stressEnd: 50,
+				      focusEnd: 50,
+				},
+		bonuses: {gameDesign: 0,
+				 programming: 0,
+				 graphics: 0,
+				 music: 0
+				 },
+		phrases: {
+			random: ["blah1", "blah2", "blah3"],
+			hungry: ["blah1", "blah2", "blah3"],
+			thirst: ["blah1", "blah2", "blah3"],
+			sleep: ["blah1", "blah2", "blah3"],
+			stress: ["blah1", "blah2", "blah3"],
+			focus: ["blah1", "blah2", "blah3"],
+		},
+		relationships: {generic1: 1,
+				 programer1: 1,
+				 gamedesigner1: 1,
+				 music1: 1		 
+				},
+		inventory: ["laptop"]
+	},
+	
